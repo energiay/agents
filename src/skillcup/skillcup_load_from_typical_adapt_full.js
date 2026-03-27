00005187ad599ad2696db658855e612f422e1ac8
itulinov <energiay@gmail.com>
2026-03-27 16:13:04 +0300

    rename

--- src/skillcup/skillcup_load_from_typical_adapt_full.js
+++ src/skillcup/skillcup_load_from_typical_adapt_full.js
@@ -6,7 +6,7 @@
 function addLog(value, name) {
     var sLogName = name
     if (sLogName == undefined) {
-        sLogName = "load_sc_from_typical_adapt_full"
+        sLogName = "skillcup_load_from_typical_adapt_full"
     }
 
     EnableLog(sLogName)
